a28e9c165027fdc2c1dc9168c33edd8dac149af9
Linna <linnali577@gmail.com>
2021-05-07 21:36:16 -0400

    O(n) implementation of Move Element to End

--- algoExpert/MoveElementToEnd.py
+++ algoExpert/MoveElementToEnd.py
@@ -18,3 +18,29 @@ def moveElementToEnd(array, toMove):
             array[i] = temp
             idx -= 1
     return array
+
+# O(n) time and O(1) space
+
+
+def moveElementToEnd2(array, toMove):
+    left = 0
+    right = len(array) - 1
+    while right > left:
+        if array[left] == toMove:
+            if array[right] != toMove:
+                swap(left, right, array)
+            else:
+                right -= 1
+        elif array[left] != toMove:
+            if array[right] != toMove:
+                left += 1
+            else:
+                left += 1
+                right -= 1
+    return array
+
+
+def swap(left, right, array):
+    temp = array[left]
+    array[left] = array[right]
+    array[right] = temp
