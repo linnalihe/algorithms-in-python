b0ec6a10fd05d3631d8120638571551a3bf98864
Linna <linnali577@gmail.com>
2021-06-03 23:20:33 -0400

    implement delete_middle_node

---
+++ crackingCodingInterview/linked_lists/delete_middle_node.py
@@ -0,0 +1,11 @@
+# Delete Middle Node
+# Implement an algorithm to delete a node in the middle;
+# You are only given access to that node
+
+def delete_node(node: Node):
+    if(node == None or node.next == None):
+        return False  # error case
+    next = node.next
+    node.value = next.value
+    n.next = next.next
+    return True
