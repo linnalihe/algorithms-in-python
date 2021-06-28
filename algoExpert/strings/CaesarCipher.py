639eab5e5e7dea9e05956af39be67b8599df105a
Linna <linnali577@gmail.com>
2021-06-28 00:27:18 -0400

    added code to caesarcipher file

--- algoExpert/strings/CaesarCipher.py
+++ algoExpert/strings/CaesarCipher.py
@@ -1,6 +1,6 @@
 def caesarCipherEncryptor(string, key):
     # Write your code here.
-	newString = [ord(char) for char in string]
-	for i in range(len(newString)):
-		newString[i] = chr((newString[i] - ord('a') + key) % 26 + ord('a'))
+    newString = [ord(char) for char in string]
+    for i in range(len(newString)):
+        newString[i] = chr((newString[i] - ord('a') + key) % 26 + ord('a'))
     return "".join(newString)
