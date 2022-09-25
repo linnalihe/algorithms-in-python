b6bab688d68e119503b4468cb49ceb6dcf03fa33
Linna Li <linnali@Linnas-MacBook-Air.local>
2022-09-25 16:04:58 -0400

    next question find closest value bst queued

---
+++ solutions/test_find_closest_value_bst.py
@@ -0,0 +1,12 @@
+# ---
+# difficulty: easy
+# category: binary tree search
+# name: find closest value in bst
+# time:
+# space: 
+# ---
+
+
+def find_closest_value_bst():
+    # code here
+    pass
