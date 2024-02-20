5f41f0a7681bd1a8976490a8fbaad32c6586d85d
Linna Li <linnali577@gmail.com>
2024-02-20 18:33:05 -0500

    2 partitions with lomuto

---
+++ solutions/2024_02_20_sorting_separate_even_and_odds.py
@@ -0,0 +1,18 @@
+# https://uplevel.interviewkickstart.com/resource/submissions/rc-codingproblem-573730-921950-1127-6897
+# all tests passed
+
+def segregate_evens_and_odds(numbers):
+    """
+    Args:
+     numbers(list_int32)
+    Returns:
+     list_int32
+    """
+    # lomuto's partition for 2 partitions
+    left = -1
+    for idx in range(len(numbers)):
+        if numbers[idx] % 2 == 0:
+            left+=1
+            numbers[idx], numbers[left] = numbers[left], numbers[idx]
+            
+    return numbers
