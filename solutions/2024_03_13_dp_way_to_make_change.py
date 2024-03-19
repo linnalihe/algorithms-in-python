e1c0c293798a11c8c9dc622bc992a21e00c5ae65
Linna Li <linnali577@gmail.com>
2024-03-19 01:10:18 -0400

    update gitignore

--- solutions/2024_03_13_dp_way_to_make_change.py
+++ solutions/2024_03_13_dp_way_to_make_change.py
@@ -2,8 +2,6 @@
 # https://uplevel.interviewkickstart.com/resource/submissions/rc-codingproblem-573753-958553-248-1566
 # all tests passed
 def number_of_ways(coins, amount):
-
-
     # top down is to recursively count by including coin at idx and excluding coin at index
     # base case is if target < 0 return 0; if idx < 0 and target is 0 return 1
     # bottom up dp is by using a matrix where first column rep number of ways to make 0 which is 1
